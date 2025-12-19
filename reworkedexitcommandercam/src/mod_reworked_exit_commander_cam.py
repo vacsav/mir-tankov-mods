f365ada7610449e5c04b2889ae350d538f0ecb62
vacsav <Vaccaria.OW@gmail.com>
2025-12-19 10:25:01 +0800

    Code refactoring

--- reworkedexitcommandercam/src/mod_reworked_exit_commander_cam.py
+++ reworkedexitcommandercam/src/mod_reworked_exit_commander_cam.py
@@ -32,7 +32,7 @@ def overrideIn(cls, condition=lambda : True):
 
 
 @overrideIn(ArcadeCamera, condition=isClientLesta)
-def enable(func, self, preferredPos = None, closesDist = False, postmortemParams = None, turretYaw = None, gunPitch = None, camTransitionParams = None, initialVehicleMatrix = None, arcadeState = None):
+def enable(func, self, preferredPos=None, closesDist=False, postmortemParams=None, turretYaw=None, gunPitch=None, camTransitionParams=None, initialVehicleMatrix=None, arcadeState=None):
     replayCtrl = BattleReplay.g_replayCtrl
     if replayCtrl.isRecording:
         replayCtrl.setAimClipPosition(self._ArcadeCamera__aimOffset)
@@ -42,58 +42,59 @@ def enable(func, self, preferredPos = None, closesDist = False, postmortemParams
     if player.observerSeesAll() and player.arena.period == constants.ARENA_PERIOD.BATTLE and vehicle and vehicle.id == player.playerVehicleID:
         self.delayCallback(0.0, self.enable, preferredPos, closesDist, postmortemParams, turretYaw, gunPitch, camTransitionParams, initialVehicleMatrix)
         return
-    elif initialVehicleMatrix is None:
-        initialVehicleMatrix = player.getOwnVehicleMatrix(Math.Matrix(self.vehicleMProv)) if vehicle is None else vehicle.matrix
-    vehicleMProv = initialVehicleMatrix
-    isPreCommanderCam = self._ArcadeCamera__compareCurrStateSettingsKey(GAME.PRE_COMMANDER_CAM)
-    isCommanderCam = self._ArcadeCamera__compareCurrStateSettingsKey(GAME.COMMANDER_CAM)
-    if isPreCommanderCam or isCommanderCam or arcadeState is not None:
-        state = None
-        newCameraDistance = self._cfg['distRange'].max
-        if arcadeState is not None:
-            self._ArcadeCamera__zoomStateSwitcher.switchToState(arcadeState.zoomSwitcherState)
-            state = self._ArcadeCamera__zoomStateSwitcher.getCurrentState()
-            newCameraDistance = arcadeState.camDist
-        self._ArcadeCamera__updateProperties(state = state)
-        self._ArcadeCamera__updateCameraSettings(newCameraDistance)
-        self._ArcadeCamera__inputInertia.glideFov(self._ArcadeCamera__calcRelativeDist())
-        if arcadeState is None:
-            self._ArcadeCamera__aimingSystem.aimMatrix = self._ArcadeCamera__calcAimMatrix()
-    camDist = None
-    if self._ArcadeCamera__postmortemMode:
-        if postmortemParams is not None:
-            self._ArcadeCamera__aimingSystem.yaw = postmortemParams[0][0]
-            self._ArcadeCamera__aimingSystem.pitch = postmortemParams[0][1]
-            camDist = postmortemParams[1]
-        else:
-            camDist = self._ArcadeCamera__distRange.max
-    elif closesDist:
-        camDist = self._ArcadeCamera__distRange.min
-    replayCtrl = BattleReplay.g_replayCtrl
-    if replayCtrl.isPlaying and not replayCtrl.isServerSideReplay:
-        camDist = None
-        vehicle = BigWorld.entity(replayCtrl.playerVehicleID)
-        if vehicle is not None:
-            vehicleMProv = vehicle.matrix
-    if camDist is not None:
-        self.setCameraDistance(camDist)
     else:
-        self._ArcadeCamera__inputInertia.teleport(self._ArcadeCamera__calcRelativeDist())
-    self.vehicleMProv = vehicleMProv
-    self._ArcadeCamera__setDynamicCollisions(True)
-    self._ArcadeCamera__aimingSystem.enable(preferredPos, turretYaw, gunPitch)
-    self._ArcadeCamera__aimingSystem.aimMatrix = self._ArcadeCamera__calcAimMatrix()
-    if camTransitionParams is not None and BigWorld.camera() is not self._ArcadeCamera__cam:
-        cameraTransitionDuration = camTransitionParams.get('cameraTransitionDuration', -1)
-        if cameraTransitionDuration > 0:
-            self._ArcadeCamera__setupCameraTransition(cameraTransitionDuration)
+        if initialVehicleMatrix is None:
+            initialVehicleMatrix = player.getOwnVehicleMatrix(Math.Matrix(self.vehicleMProv)) if vehicle is None else vehicle.matrix
+        vehicleMProv = initialVehicleMatrix
+        isCommanderCam = self._ArcadeCamera__compareCurrStateSettingsKey(GAME.PRE_COMMANDER_CAM) or self._ArcadeCamera__compareCurrStateSettingsKey(GAME.COMMANDER_CAM)
+        if isCommanderCam or arcadeState is not None:
+            state = None
+            newCameraDistance = self._cfg['distRange'].max
+            if arcadeState is not None:
+                self._ArcadeCamera__zoomStateSwitcher.switchToState(arcadeState.zoomSwitcherState)
+                state = self._ArcadeCamera__zoomStateSwitcher.getCurrentState()
+                newCameraDistance = arcadeState.camDist
+            self._ArcadeCamera__updateProperties(state = state)
+            self._ArcadeCamera__updateCameraSettings(newCameraDistance)
+            self._ArcadeCamera__inputInertia.glideFov(self._ArcadeCamera__calcRelativeDist())
+            if arcadeState is None:
+                self._ArcadeCamera__aimingSystem.aimMatrix = self._ArcadeCamera__calcAimMatrix()
+        camDist = None
+        if self._ArcadeCamera__postmortemMode:
+            if postmortemParams is not None:
+                self._ArcadeCamera__aimingSystem.yaw = postmortemParams[0][0]
+                self._ArcadeCamera__aimingSystem.pitch = postmortemParams[0][1]
+                camDist = postmortemParams[1]
+            else:
+                camDist = self._ArcadeCamera__distRange.max
+        elif closesDist:
+            camDist = self._ArcadeCamera__distRange.min
+        replayCtrl = BattleReplay.g_replayCtrl
+        if replayCtrl.isPlaying and not replayCtrl.isServerSideReplay:
+            camDist = None
+            vehicle = BigWorld.entity(replayCtrl.playerVehicleID)
+            if vehicle is not None:
+                vehicleMProv = vehicle.matrix
+        if camDist is not None:
+            self.setCameraDistance(camDist)
+        else:
+            self._ArcadeCamera__inputInertia.teleport(self._ArcadeCamera__calcRelativeDist())
+        self.vehicleMProv = vehicleMProv
+        self._ArcadeCamera__setDynamicCollisions(True)
+        self._ArcadeCamera__aimingSystem.enable(preferredPos, turretYaw, gunPitch)
+        self._ArcadeCamera__aimingSystem.aimMatrix = self._ArcadeCamera__calcAimMatrix()
+        if camTransitionParams is not None and BigWorld.camera() is not self._ArcadeCamera__cam:
+            cameraTransitionDuration = camTransitionParams.get('cameraTransitionDuration', -1)
+            if cameraTransitionDuration > 0:
+                self._ArcadeCamera__setupCameraTransition(cameraTransitionDuration)
+            else:
+                self._ArcadeCamera__setCamera()
         else:
             self._ArcadeCamera__setCamera()
-    else:
-        self._ArcadeCamera__setCamera()
-    self._ArcadeCamera__cameraUpdate()
-    self.delayCallback(0.0, self._ArcadeCamera__cameraUpdate)
-    from gui import g_guiResetters
-    g_guiResetters.add(self._ArcadeCamera__onRecreateDevice)
-    self._ArcadeCamera__updateAdvancedCollision()
-    self._ArcadeCamera__updateLodBiasForTanks()
+        self._ArcadeCamera__cameraUpdate()
+        self.delayCallback(0.0, self._ArcadeCamera__cameraUpdate)
+        from gui import g_guiResetters
+        g_guiResetters.add(self._ArcadeCamera__onRecreateDevice)
+        self._ArcadeCamera__updateAdvancedCollision()
+        self._ArcadeCamera__updateLodBiasForTanks()
+        return
